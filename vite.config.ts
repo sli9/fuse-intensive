7bf2ab68ddd78cddbb1d9bb24aba087a0aa0dc25
sli9 <sil9gosy@mail.ru>
2025-03-09 11:59:11 +0300

    add aliases for imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@shared': '/src/shared',
+      '@app': '/src/app',
+      '@pages': '/src/pages',
+    },
+  },
   plugins: [react()],
 });
