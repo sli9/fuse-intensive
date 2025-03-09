4abace16543d1f864b0e99b0d5067e51669d9017
sli9 <sil9gosy@mail.ru>
2025-03-09 12:33:35 +0300

    add main and random post pages

---
+++ src/pages/main/ui/Main.tsx
@@ -0,0 +1,8 @@
+import s from './main.module.scss';
+export const Main = () => {
+  return (
+    <div className={s.container}>
+      <h1>Главная</h1>
+    </div>
+  );
+};
---
+++ src/pages/random-post/ui/RandomPost.tsx
@@ -0,0 +1,10 @@
+import s from './randomPost.module.scss';
+
+export const RandomPost = () => {
+  return (
+    <div className={s.container}>
+      <h1>Рандомный пост</h1>
+      <h3>post header</h3>
+    </div>
+  );
+};
